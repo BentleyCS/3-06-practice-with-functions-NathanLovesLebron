295d659370ff9f259609fa7782e1e6345d3f2d41
NathanLovesLebron <nlegan28@bentleyschool.org>
2025-11-17 14:11:07 -0800

    Add files via upload

--- CSP_3_06_Practice_with_functions.py
+++ CSP_3_06_Practice_with_functions.py
@@ -7,38 +7,58 @@ def root(n):
 
 #Takes in the 3 side lengths of a triangle as arguments and returns half of
 #the perimeter of a triangle.
-def semiPerimeter():
-    pass
+def semiPerimeter(a,b,c):
+
+    x=int((a)+(b)+(c))
+    return (x/2)
+
 
 #Modify the below function such that it takes in 4 arguments. multiply the first
 #argument by the difference between itself and each individual argument. Reference herons formula for more context.
-def multiplyDifferences():
-    pass
+def multiplyDifferences(a,b,c,d):
+    return (a*(a-b)*(a-c)*(a-d))
+
 
 #Given the 3 sides of a triangle return the area.
 #use herons formula
 #Use the functions above.
-def herons():
-    pass
+def herons(a,b,c):
+    s = semiPerimeter(a,b,c)
+    area = multiplyDifferences(s, a ,b ,c)
 
+    return root(area)
 
 #quadratic equation
 
 #takes in a number as an argument and returns that number multiplied by 2.
-def denominator():
-    pass
+def denominator(a):
+    New=(a*2)
+    return(New)
 
 #Takes in two arguments. multiply the first argument by negative 1. Then
 #return the modified first argument added and subtracted by the second argument.
-def plusMinus():
-    pass
+def plusMinus(a,b):
+    x=(a*-1)
+    y=(x-b)
+    z=(x+b)
+    return(y,z)
 #takes in three numbers as arguments. The first and third multiplied together then
 #multiplied by 4.Then subtract that result from the second argument squared.
 #Return the overall result.
-def mainCalc():
-    pass
+def mainCalc(a,b,c):
+    x=(a*c)
+    y=(x*4)
+    n=(b*b)
+    z=(n-y)
+    return(z)
 
 #The below function should take the inputs of the quadratic equation and return the result
 #Make sure to use all the formulas from this section.
-def quadratic():
-    pass
+def quadratic(a,b,c):
+    d = denominator(a)
+    m = mainCalc(a,b,c)
+    m = root(m)
+    x, y = plusMinus(b,m)
+    x = x/d
+    y /= d
+    return(x,y)
